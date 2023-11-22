352f0a22ba4f5b857d1a6103cfb44b3d423be62f
ENNA-Egor <e.yudakov@gmail.com>
2023-11-22 09:07:43 +0300

    Создал основной интерфейс

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 
-import './App.css';
+import {Header}  from './layout/Header'
+import {Footer}  from './layout/Footer'
+import {Main}  from './layout/Main'
 
 function App() {
   return (
-    <div className="App">
-          Hello react App
-    </div>
+    <>
+      < Header />
+      < Main />
+      < Footer />
+    </>
   );
 }
 
