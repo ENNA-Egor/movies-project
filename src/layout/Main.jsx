4fd69cb8c121cbc348dce9437e542de6232a03e8
ENNA-Egor <e.yudakov@gmail.com>
2023-11-23 09:06:02 +0300

    Почистил код

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,11 +1,9 @@
 import {Movies} from '../components/Movies'
-import {Movie} from '../components/Movie'
 
 function Main () {
     return <main className="container content">
         Yello from Movies!!
         <Movies />
-        {/* <Movie /> */}
 
     </main>
 }
