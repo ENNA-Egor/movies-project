ce3f2b309672b5caa45f020267c4037dd4439d89
ENNA-Egor <e.yudakov@gmail.com>
2023-11-28 14:35:48 +0300

    Добаилен поиск

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Movies} from '../components/Movies';
 import { Preloader } from '../components/Preloader';
+import { Search } from '../components/Search';
 
 class Main extends React.Component {
     state = {
@@ -12,9 +13,17 @@ class Main extends React.Component {
         then(response => response.json())
         .then(data => this.setState({movies: data.Search}))
     }
+
+    searchMovies = (str ) =>{
+        fetch(`http://www.omdbapi.com/?apikey=2830a6ee&s=${str}`).
+        then(response => response.json())
+        .then(data => this.setState({movies: data.Search}))
+    }
+
     render() {
         const {movies} = this.state;
         return <main className="container content">
+            < Search searchMovies={this.searchMovies}/>
         {
             movies.length ? (   
              <Movies movies={this.state.movies}/>
